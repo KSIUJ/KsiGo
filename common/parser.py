861fdffbde672910253b0dc717120e1df52271a4
zwatotem <zwatotem@gmail.com>
2020-09-13 17:26:06 +0200

    Reimplement parser with pickle library

--- common/parser.py
+++ common/parser.py
@@ -1,52 +1,58 @@
+import pickle
+
+
+class Message:
+    def __init__(self, func, args: tuple):
+        super().__init__()
+        self.func = func
+        self.args = args
+
+    # def __getstate__(self) -> dict:
+    #     return self.__dict__.copy()
+
+    # def __setstate__(self, state: dict):
+    #     self.__dict__.update(state)
+
+    def print(self):
+        return "Message: " + self.args
+
+
 class Parser:
-    separator = ""
-    binds = {}
     @classmethod
-    def execute(cls, command: str):
-        (pref, _, suff) = command.partition(cls.separator)
-        cls.binds[pref](suff)
-
-    @staticmethod
-    def remove_parenthesis(string: str) -> str:
-        if string.startswith("("):
-            string = string.lstrip("(")
-        if string.endswith(")"):
-            string = string.rstrip(")")
-        return string
+    def execute(cls, command: bytes):
+        command = cls.decode(command)
+        command.func(command.args)
 
     @classmethod
-    def extract_args(cls, types: tuple, arg_string: str) -> list:
-        ret_tuple = list()
-        arg_string = cls.remove_parenthesis(arg_string)
-        for type_ in types:
-            (arg, _, arg_string) = arg_string.partition(",")
-            arg = arg.strip()
-            arg_string = arg_string.strip()
-            ret_tuple.append(type_(arg))
-        return ret_tuple
+    def encode(cls, mssg: Message) -> bytes:
+        return pickle.dumps(mssg)
 
-class GameParserHelper:
     @classmethod
-    def place(cls, args: str):
-        [x, y] = Parser.extract_args((int, int), args)
-        print(f"This command places a stone on a board at coordinates ({x}, {y})")
+    def decode(cls, mssg: bytes) -> Message:
+        return pickle.loads(mssg)
 
-class GameParser(Parser):
-    separator = "("
-    binds = {
-        "place": GameParserHelper.place
-    }
+
+class GameCommandManager(Parser):
     @classmethod
-    def place(cls, x: int,y: int) -> str:
-        return "game.place({x}, {y})"
+    def place(cls, args: tuple):
+        (x, y) = args
+        print(
+            f"This command places a stone on a board at coordinates ({x}, {y})")
+
+    @classmethod
+    def encode_place(cls, x: int, y: int) -> bytes:
+        return cls.encode(Message(GameCommandManager.place, (x, y)))
+
+    @classmethod
+    def encode_somecomplicatedfunc(cls, x: int, s: str, y: int):
+        return cls.encode(Message(GameCommandManager.somecomplicatedfunc, (x, s, y)))
+
+    @classmethod
+    def somecomplicatedfunc(cls, args):
+        (x, s, y) = args
+        print(
+            f"This function places a {s} stone on a board at coordinates ({x}, {y})")
 
 
-class MainParser(Parser):
-    separator = "."
-    parsers = {
-        "game": GameParser
-    }
-    # binds = {string: parser.execute for (string, parser) in parsers.items()}
-    binds = {
-        "game": GameParser.execute
-    }
+Parser.execute(GameCommandManager.encode_somecomplicatedfunc(
+    12, "", 13))
