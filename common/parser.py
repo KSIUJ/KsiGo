b2e119775c3d773ea7d0f2513c8481da64277ba0
zwatotem <zwatotem@gmail.com>
2020-09-24 18:45:30 +0200

    Parser: add runtime configurable function bindings

--- common/parser.py
+++ common/parser.py
@@ -1,34 +1,66 @@
 import pickle
+import typing
+
+
+class ParsingError(KeyError):
+    def __init__(self):
+        super().__init__(self)
 
 
 class Message:
-    def __init__(self, func, args: tuple):
+    def __init__(self, module: str, func: str, args: tuple):
+        self.module = module
         self.func = func
         self.args = args
 
 
 class Parser:
-    @classmethod
-    def execute(cls, command: bytes):
-        command = cls.decode(command)
-        command.func(command.args)
+    def __init__(self):
+        self.bindings = {}
 
-    @classmethod
-    def encode(cls, mssg: Message) -> bytes:
+    def add_module(self, module_name: str):
+        self.bindings[module_name] = {}
+
+    def add_func(self, module: str, func_name: str, func: typing.Callable):
+        self.bindings[module][func_name] = func
+
+    def execute(self, command: bytes):
+        command = self.decode(command)
+        try:
+            self.bindings[command.module][command.func](command.args)
+        except KeyError as e:
+            pe = ParsingError(e)
+            raise pe
+
+    def encode(self, mssg: Message) -> bytes:
         return pickle.dumps(mssg)
 
-    @classmethod
-    def decode(cls, mssg: bytes) -> Message:
+    def encode_message(self, module: str, func: str, args: tuple) -> bytes:
+        return self.encode(Message(module, func, args))
+
+    def decode(self, mssg: bytes) -> Message:
         return pickle.loads(mssg)
 
+# Example code that uses Parser
 
-class GameCommandManager(Parser):
-    @classmethod
-    def place(cls, args: tuple):
+
+class GameCommandManager:
+    def __init__(self):
+        parser = Parser()
+        parser.add_module("game")
+        parser.add_func("game", "place", self.place)
+        self.parser = parser
+
+    @staticmethod
+    def place(args: tuple):
         (x, y) = args
         print(
             f"This command places a stone on a board at coordinates ({x}, {y})")
 
-    @classmethod
-    def encode_place(cls, x: int, y: int) -> bytes:
-        return cls.encode(Message(GameCommandManager.place, (x, y)))
+    def encode_place(self, x: int, y: int) -> bytes:
+        return self.parser.encode_message("game", "place", (x, y))
+
+
+if __name__ == "__main__":
+    gcm = GameCommandManager()
+    gcm.parser.execute(gcm.encode_place(12, 13))
